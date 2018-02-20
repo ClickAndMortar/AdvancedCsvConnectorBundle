c574100847e6159d14e45a431da796887a4a97ea
Simon CARRE <simoncarre54@gmail.com>
2018-02-20 16:31:51 +0100

    Add mapping validation before file reading

--- Reader/File/Csv/ProductAdvancedReader.php
+++ Reader/File/Csv/ProductAdvancedReader.php
@@ -5,6 +5,8 @@ namespace ClickAndMortar\AdvancedCsvConnectorBundle\Reader\File\Csv;
 use Akeneo\Component\Batch\Item\FileInvalidItem;
 use Akeneo\Component\Batch\Item\InitializableInterface;
 use Akeneo\Component\Batch\Item\InvalidItemException;
+use Akeneo\Component\Batch\Job\BatchStatus;
+use Akeneo\Component\Batch\Job\ExitStatus;
 use ClickAndMortar\AdvancedCsvConnectorBundle\Helper\ImportHelper;
 use Pim\Component\Connector\ArrayConverter\ArrayConverterInterface;
 use Pim\Component\Connector\Exception\DataArrayConversionException;
@@ -20,6 +22,34 @@ use Pim\Component\Connector\Reader\File\MediaPathTransformer;
  */
 class ProductAdvancedReader extends ProductReader implements InitializableInterface
 {
+    /**
+     * Mapping base attributes key
+     *
+     * @var string
+     */
+    const MAPPING_BASE_ATTRIBUTES_KEY = 'attributes';
+
+    /**
+     * Attribute code key for each attribute in mapping
+     *
+     * @var string
+     */
+    const MAPPING_ATTRIBUTE_CODE_KEY = 'attributeCode';
+
+    /**
+     * Column code key in mapping
+     *
+     * @var string
+     */
+    const MAPPING_DATA_CODE_KEY = 'dataCode';
+
+    /**
+     * Callback key in mapping
+     *
+     * @var string
+     */
+    const MAPPING_CALLBACK_KEY = 'callback';
+
     /**
      * Import helper
      *
@@ -94,6 +124,7 @@ class ProductAdvancedReader extends ProductReader implements InitializableInterf
         if (empty($this->mapping)) {
             $mappingAsJson = $jobParameters->get('mapping');
             $this->mapping = json_decode($mappingAsJson, true);
+            $this->validateMapping();
         }
 
         if (!empty($this->waitingListCsvFilesPaths)) {
@@ -185,30 +216,56 @@ class ProductAdvancedReader extends ProductReader implements InitializableInterf
     protected function updateByMapping($item)
     {
         $newItem = [];
-        foreach ($this->mapping['attributes'] as $attributeMapping) {
+        foreach ($this->mapping[self::MAPPING_BASE_ATTRIBUTES_KEY] as $attributeMapping) {
             $value = null;
 
             // Simple mapping
-            if (isset($attributeMapping['dataCode'])) {
-                $value = $this->importHelper->getByCode($item, $attributeMapping['dataCode']);
+            if (isset($attributeMapping[self::MAPPING_DATA_CODE_KEY])) {
+                $value = $this->importHelper->getByCode($item, $attributeMapping[self::MAPPING_DATA_CODE_KEY]);
             }
 
             // Add value in new item
             if ($value !== null) {
                 // Update value by callback method if necessary
-                if (isset($attributeMapping['callback'])) {
-                    if (method_exists($this->importHelper, $attributeMapping['callback'])) {
-                        $value = $this->importHelper->{$attributeMapping['callback']}($value, $attributeMapping['attributeCode']);
+                if (isset($attributeMapping[self::MAPPING_CALLBACK_KEY])) {
+                    if (method_exists($this->importHelper, $attributeMapping[self::MAPPING_CALLBACK_KEY])) {
+                        $value = $this->importHelper->{$attributeMapping[self::MAPPING_CALLBACK_KEY]}($value, $attributeMapping[self::MAPPING_ATTRIBUTE_CODE_KEY]);
                     } else {
-                        $this->throwInvalidItemException($item, 'batch_jobs.csv_advanced_product_import.import.warnings.no_callback_method', ['%callbackMethod%' => $attributeMapping['callback']]);
+                        $this->throwInvalidItemException($item, 'batch_jobs.csv_advanced_product_import.import.warnings.no_callback_method', ['%callbackMethod%' => $attributeMapping[self::MAPPING_CALLBACK_KEY]]);
                     }
                 }
 
-                $newItem[$attributeMapping['attributeCode']] = $value;
+                $newItem[$attributeMapping[self::MAPPING_ATTRIBUTE_CODE_KEY]] = $value;
             }
         }
 
         return $newItem;
+    }
+
+    /**
+     * Validate current mapping
+     *
+     * @return bool
+     */
+    protected function validateMapping()
+    {
+        // Check for main attributes key
+        if (!isset($this->mapping[self::MAPPING_BASE_ATTRIBUTES_KEY])) {
+            $this->stopStepExecution('batch_jobs.csv_advanced_product_import.import.errors.mapping_attributes_error');
+
+            return false;
+        }
+
+        // Check each attribute mapping
+        foreach ($this->mapping[self::MAPPING_BASE_ATTRIBUTES_KEY] as $attributeMapping) {
+            if (!isset($attributeMapping[self::MAPPING_ATTRIBUTE_CODE_KEY])) {
+                $this->stopStepExecution('batch_jobs.csv_advanced_product_import.import.errors.mapping_no_attribute_code');
+
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -228,5 +285,20 @@ class ProductAdvancedReader extends ProductReader implements InitializableInterf
             ($this->stepExecution->getSummaryInfo('item_position'))
         );
         throw new InvalidItemException($message, $invalidItem, $parameters);
+    }
+
+    /**
+     * Stop step execution with error message
+     *
+     * @param string $errorMessage
+     *
+     * @return void
+     */
+    protected function stopStepExecution($errorMessage = '')
+    {
+        $this->stepExecution->setStatus(new BatchStatus(BatchStatus::FAILED));
+        $this->stepExecution->setExitStatus(new ExitStatus(ExitStatus::FAILED));
+        $this->stepExecution->setEndTime(new \DateTime('now'));
+        $this->stepExecution->addFailureException(new \Exception($errorMessage));
     }
 }
