3ade4d8618b7ac842e73700463bfcdccc3ebe110
Simon CARRE <simoncarre54@gmail.com>
2022-04-26 12:31:21 +0200

    Add custom product column sorter by mapping

---
+++ Writer/File/ProductColumnSorterByMapping.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ClickAndMortar\AdvancedCsvConnectorBundle\Writer\File;
+
+use Akeneo\Pim\Enrichment\Component\Product\Connector\ProductColumnSorter;
+
+/**
+ * Reorder columns by mapping
+ *
+ * @author  Simon CARRE <simon.carre@clickandmortar.fr>
+ * @package ClickAndMortar\AdvancedCsvConnectorBundle\Writer\File
+ */
+class ProductColumnSorterByMapping extends ProductColumnSorter
+{
+    /**
+     * Columns order context key
+     *
+     * @var string
+     */
+    const CONTEXT_KEY_COLUMNS_ORDER = 'columnsOrder';
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function sort(array $columns, array $context = [])
+    {
+        // Get order from context if possible
+        if (isset($context[self::CONTEXT_KEY_COLUMNS_ORDER])) {
+            $sortedColumns = [];
+            foreach ($context[self::CONTEXT_KEY_COLUMNS_ORDER] as $columnName) {
+                if (in_array($columnName, $columns)) {
+                    $sortedColumns[] = $columnName;
+                }
+            }
+
+            return $sortedColumns;
+        }
+
+        // Else keep classic case
+        return parent::sort($columns, $context);
+    }
+}
